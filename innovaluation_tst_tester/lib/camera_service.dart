0627319aa5674f44151af75224e0de2cf793586b
lumamy310 <lumamy310@gmail.com>
2024-03-08 19:11:00 -0500

    Adding geolocation data to images.

--- innovaluation_tst_tester/lib/camera_service.dart
+++ innovaluation_tst_tester/lib/camera_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
@@ -7,6 +8,7 @@ import 'package:innovaluation_tst_tester/main_menu_screen.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:innovaluation_tst_tester/theme_data.dart';
+import 'package:geolocator/geolocator.dart';
 
 Future<void> main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
@@ -173,7 +175,7 @@ class DisplayPictureScreen extends StatelessWidget {
 
   const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
 
-  Future<void> _uploadImageToFirestore() async {
+  Future<void> _uploadImageToFirestore(Position position) async {
     // Get the file from the imagePath
     File file = File(imagePath);
 
@@ -194,6 +196,47 @@ class DisplayPictureScreen extends StatelessWidget {
       'url': downloadURL,
       'createdAt': FieldValue.serverTimestamp(),
     });
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 
   @override
@@ -219,7 +262,7 @@ class DisplayPictureScreen extends StatelessWidget {
                           content: Text('Uploading image'),
                         )
                     );
-                    await _uploadImageToFirestore();
+                    await _uploadImageToFirestore(await _determinePosition());
                     ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
                           content: Text('Uploaded'),
@@ -231,8 +274,7 @@ class DisplayPictureScreen extends StatelessWidget {
                     );
                   } catch (error) {
                     // Handle error
-                    print('Error uploading image: $error');
-                    // Show an error message to the user
+                    log('$error', name: 'MyApp', error: 'Something went wrong');                    // Show an error message to the user
                     ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
                         content: Text('Failed to upload image. Please try again.'),
